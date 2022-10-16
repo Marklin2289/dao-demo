a3b5e4404c500efa2a5f0346fcde08dafdfc47be
marklin2289 <marklin2289@gmail.com>
2022-10-15 21:24:14 -0700

    yarn hardhat compile fixed

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
@@ -20,10 +22,10 @@ contract GovernanceToken is ERC20Votes {
     }
 
     function _mint(address to, uint256 amount) internal override(ERC20Votes) {
-        super._mint(to, amount)
+        super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount) internal override(ERC20Votes) {
-        super._burn(account, amount)
+        super._burn(account, amount);
     }
 }
