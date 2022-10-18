b5363131c524d2e45df186dfb067489c02b92aaa
marklin2289 <marklin2289@gmail.com>
2022-10-18 13:15:08 -0700

    added 01-deploy-governor-token.ts

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,5 +1,8 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { developmentChains, networkConfig } from "../helper-hardhat-config"
+import { ethers } from "hardhat"
+import verify from "../helper-functions"
 
 const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { getNamedAccounts, deployments, network } = hre
@@ -7,10 +10,27 @@ const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntim
     const { deployer } = await getNamedAccounts()
     log("------------------------------------------------------")
     log("Deploying deployGovernanceToken and waiting for confirmation...")
-    const governanceToken = await deploy("governanceToken", {
+    const governanceToken = await deploy("GovernanceToken", {
         from: deployer,
         args: [],
         log: true,
         waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
     })
+    log(`GovernanceToken at : ${governanceToken.address}`)
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(governanceToken.address, [])
+    }
+    log(`Delegating to ${deployer}`)
+    await delegate(governanceToken.address, deployer)
+    log("Delegated!")
 }
+
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+    const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress)
+    const tx = await governanceToken.delegate(delegatedAccount)
+    await tx.wait(1)
+    console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`)
+}
+
+export default deployGovernanceToken
+deployGovernanceToken.tags = ["all", "governor"]
